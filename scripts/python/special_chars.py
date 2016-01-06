a8b02c417e4770b77bc87d85ffb9222645af8f47
Hucaru <n/a>
2016-01-06 20:23:18 +0000

    Modified special characters script to search for xml from a root directory provided

--- scripts/python/special_chars.py
+++ scripts/python/special_chars.py
@@ -1,18 +1,26 @@
-import codecs
+import codecs, os, sys
 from lxml import etree as et
 
-xml_file = ""
+root_dir = sys.argv[1]
+xml_files = []
 
-parser = et.XMLParser(ns_clean=False, recover=True, remove_comments=False, remove_blank_text=False, encoding='utf-8')
-tree = et.parse(xml_file, parser=parser)
+for root, dirs, files in os.walk(root_dir):
+    for file in files:
+        if file.endswith(".xml"):
+            xml_files.insert(-1, os.path.join(root, file))
 
-print("Replacing")
-for i in tree.iter():
-    for k, v in i.attrib.iteritems():
-        v = v.replace(u'\u2019', '\'')
-        i.attrib[k] = v
+for xml_file in xml_files:
+    parser = et.XMLParser(ns_clean=False, recover=True, remove_comments=False, remove_blank_text=False, encoding='utf-8')
+    tree = et.parse(xml_file, parser=parser)
 
-with codecs.open(xml_file, 'w', 'utf-8') as f:
-    xml_string = et.tostring(tree, encoding='UTF-8', xml_declaration=True)
-    f.write(xml_string.replace('&#10;', '\\n').replace('&#8217;', '\'').decode('utf-8'))
-print("finished")
+    print("Replacing " + xml_file)
+    for i in tree.iter():
+        for k, v in i.attrib.iteritems():
+            v = v.replace(u'\u2019', '\'')
+            i.attrib[k] = v
+
+    with codecs.open(xml_file, 'w', 'utf-8') as f:
+        xml_string = et.tostring(tree, encoding='UTF-8', xml_declaration=True)
+        if xml_string: # str.replace() errors out on null
+            f.write(xml_string.replace('&#10;', '\\n').replace('&#8217;', '\'').decode('utf-8'))
+    print("finished")
